cfdc6ccf30ba753ba158d9b9933db2e46073db85
kadzer <kadzer@gmail.com>
2018-10-07 19:08:03 +0100

    adds trumpet class

--- src/main/java/Trumpet.java
+++ src/main/java/Trumpet.java
@@ -1,2 +1,17 @@
-public class Trumpet {
+public class Trumpet extends Instrument implements IPlay{
+    private String trumpetType;
+
+    public Trumpet(InstrumentType type, String make, String model, double cost, double price, String trumpetType) {
+        super(type, make, model, cost, price);
+        this.trumpetType = trumpetType;
+    }
+
+    @Override
+    public String play() {
+        return "Doot!";
+    }
+
+    public String getTrumpetType() {
+        return trumpetType;
+    }
 }
